b2099ce7ae3897576f00caf49826284d50a18a5d
LeoViana03 <leleu.viana@hotmail.com>
2023-06-16 18:20:58 -0300

    Adicionando loop e variáveis.

--- forca.py
+++ forca.py
@@ -1,9 +1,20 @@
-
 def jogar():
-
     print("*********************************")
     print("***Bem Vindo ao Jogo da Forca!***")
     print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra ? ")
+
+        print("Jogando...")
+
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
